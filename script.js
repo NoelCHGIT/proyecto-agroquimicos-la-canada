15c1b96266c2a28d0ecbddac52fe78f62114b832
noelch082000 <noelch082000@gmail.com>
2023-06-07 05:05:50 -0600

    feat AVANCE

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ const menu = document.querySelector('.menu');
 menuToggle.addEventListener('click', () => {
     menu.classList.toggle('show');
 });
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    const sliderContainer = document.querySelector(".slider-container");
+    const slides = document.querySelectorAll(".slide");
+    const arrowLeft = document.querySelector(".arrow-left");
+    const arrowRight = document.querySelector(".arrow-right");
+    let currentSlide = 0;
+  
+    arrowLeft.addEventListener("click", function() {
+      if (currentSlide === 0) {
+        currentSlide = slides.length - 1;
+      } else {
+        currentSlide--;
+      }
+      updateSlider();
+    });
+  
+    arrowRight.addEventListener("click", function() {
+      if (currentSlide === slides.length - 1) {
+        currentSlide = 0;
+      } else {
+        currentSlide++;
+      }
+      updateSlider();
+    });
+  
+    function updateSlider() {
+      sliderContainer.style.transform = `translateX(-${currentSlide * 100}%)`;
+    }
+  
+    function autoSlide() {
+      if (currentSlide === slides.length - 1) {
+        currentSlide = 0;
+      } else {
+        currentSlide++;
+      }
+      updateSlider();
+    }
+  
+    setInterval(autoSlide, 4000); // Cambiar de imagen cada 4 segundos
+  });
+  

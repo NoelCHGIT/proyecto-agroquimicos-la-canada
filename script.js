2f80814e56949fe1faf942622994b696bd037499
noelch082000 <noelch082000@gmail.com>
2023-06-07 02:52:19 -0600

    feat primero commit

---
+++ script.js
@@ -0,0 +1,7 @@
+// JavaScript para el menú desplegable
+const menuToggle = document.querySelector('.menu-toggle');
+const menu = document.querySelector('.menu');
+
+menuToggle.addEventListener('click', () => {
+    menu.classList.toggle('show');
+});

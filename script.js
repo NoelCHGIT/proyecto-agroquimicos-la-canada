0db7b524f5672540d3054df91845a40b7130d296
noelch082000 <noelch082000@gmail.com>
2023-06-10 02:33:16 -0600

    feat AVANCE

--- script.js
+++ script.js
@@ -45,6 +45,78 @@ document.addEventListener("DOMContentLoaded", function() {
       updateSlider();
     }
   
-    setInterval(autoSlide, 4000); // Cambiar de imagen cada 4 segundos
+    setInterval(autoSlide, 8000); // Cambiar de imagen cada 8 segundos
   });
   
+
+
+// Página Catalogo
+document.addEventListener("DOMContentLoaded", function() {
+  // ...
+
+  const indexLinks = document.querySelectorAll(".index-bar a");
+
+  indexLinks.forEach(function(link) {
+      link.addEventListener("click", function(event) {
+          event.preventDefault();
+
+          const targetId = link.getAttribute("href").substring(1);
+          const targetElement = document.getElementById(targetId);
+
+          targetElement.scrollIntoView({ behavior: "smooth" });
+      });
+  });
+
+  // ...
+});
+
+// Filtrar productos por letra
+function filterByLetter(letter) {
+  // Ocultar todos los productos
+  const products = document.querySelectorAll('.product');
+  products.forEach(product => {
+      product.style.display = 'none';
+  });
+
+  // Mostrar productos que comienzan con la letra seleccionada
+  const filteredProducts = document.querySelectorAll(`[data-letter="${letter}"]`);
+  filteredProducts.forEach(product => {
+      product.style.display = 'block';
+  });
+}
+
+// Filtrar productos por marca
+function filterByBrand(brand) {
+  // Ocultar todos los productos
+  const products = document.querySelectorAll('.product');
+  products.forEach(product => {
+      product.style.display = 'none';
+  });
+
+  // Mostrar productos de la marca seleccionada
+  const filteredProducts = document.querySelectorAll(`[data-brand="${brand}"]`);
+  filteredProducts.forEach(product => {
+      product.style.display = 'block';
+  });
+}
+
+// Evento al hacer clic en una letra del índice alfabético
+const indexButtons = document.querySelectorAll('.index-button');
+indexButtons.forEach(button => {
+  button.addEventListener('click', function(event) {
+      event.preventDefault();
+      const letter = button.innerText;
+      filterByLetter(letter);
+  });
+});
+
+// Evento al hacer clic en una marca del índice por marca
+const brandLogos = document.querySelectorAll('.brand-logo');
+brandLogos.forEach(logo => {
+  logo.addEventListener('click', function(event) {
+      event.preventDefault();
+      const brand = logo.dataset.brand;
+      filterByBrand(brand);
+  });
+});
+
